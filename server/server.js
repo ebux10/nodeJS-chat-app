281ce441020bebb68a42c84388c5ef8c7189ea03
Chukwuebuka <ebeluolisa@gail.com>
2018-01-30 10:26:09 -0500

    First Commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+
+const publicPath = path.join(__dirname , "../public");
+
+var app = express();
+app.use(express.static(publicPath));
+var port = process.env.PORT || 3000;
+
+app.get("/" , (req , res)=>{
+    res.render("index.html");
+});
+
+app.listen(port , ()=>{
+    console.log(`The server is up and running on PORT :${port}`);
+})
